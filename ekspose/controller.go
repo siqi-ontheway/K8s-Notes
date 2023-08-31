8975d7e2dac6741d86395029d4807713b5fa1e03
Siqi Li <siqili1226@gmail.com>
2023-08-30 23:17:53 -0700

    add delete event logic, add retry logic

--- ekspose/controller.go
+++ ekspose/controller.go
@@ -8,13 +8,16 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	coreV1 "k8s.io/api/core/v1"
 	netV1 "k8s.io/api/networking/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	appsinformers "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
 	appslisters "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
 )
 
 type controller struct {
@@ -68,6 +71,7 @@ func (c *controller) worker() {
 }
 
 // Poll from the queue and then sync deployment
+// Production: Refer to claimWorker method in kubenetes library
 func (c *controller) processItem() bool {
 	item, shutdown := c.queue.Get()
 	if shutdown {
@@ -79,19 +83,64 @@ func (c *controller) processItem() bool {
 		fmt.Printf("getting key from cache %s\n", err.Error())
 	}
 	ns, name, err := cache.SplitMetaNamespaceKey(key)
+
 	if err != nil {
 		fmt.Printf("splitting key into namespace and name %s\n", err.Error())
 		return false
 	}
 
+	// Check if the object has been deleted from k8s cluster
+	ctx := context.Background()
+	_, err = c.clientset.AppsV1().Deployments(ns).Get(ctx, name, metaV1.GetOptions{})
+	// If error is that the object is not found in k8s cluster, i.e. the event is deletion
+	if apierrors.IsNotFound(err) {
+		fmt.Printf("deployment %s was deleted\n", name)
+		//delete services, note: here we only consider the case whne the service name is the same as the deployment name
+		err = c.clientset.CoreV1().Services(ns).Delete(ctx, name, metaV1.DeleteOptions{})
+		if err != nil {
+			fmt.Printf("deleting service %s, error %s\n", name, err.Error())
+			return false
+		}
+		//delete ingress, note: here we only consider the case whne the ingress name is the same as the deployment name
+		err = c.clientset.NetworkingV1().Ingresses(ns).Delete(ctx, name, metaV1.DeleteOptions{})
+		if err != nil {
+			fmt.Printf("deleting ingress %s, error %s\n", name, err.Error())
+			return false
+		}
+		return true
+	}
+
 	err = c.syncDeployment(ns, name)
 	if err != nil {
 		//retry
-
+		c.retry(err, item)
 		fmt.Printf("syncing deployment %s\n", err.Error())
 		return false
 	}
 	return true
+}
+
+// Retry for five times if failed to sync deployment
+func (c *controller) retry(err error, key interface{}) {
+	if err == nil {
+		// Item is successfully processed.
+		c.queue.Forget(key)
+		return
+	}
+
+	// If item is not successfully processed,
+	// check how many times you have retried until 5 times
+	if c.queue.NumRequeues(key) < 5 {
+		klog.Infof("Error syncing pod %v: %v", key, err)
+		c.queue.AddRateLimited(key)
+		return
+	}
+
+	// If you have reached the 5 limit times of retry, forget this item
+	c.queue.Forget(key)
+	// report error
+	runtime.HandleError(err)
+	klog.Infof("Dropping pod %q out of the queue: %v", key, err)
 }
 
 // Sync deployment: create service and ingress
