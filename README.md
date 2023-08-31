16e13e0e2b693306a46c9b4806b17cd0459edeea
Siqi Li <siqili1226@gmail.com>
2023-08-30 18:29:25 -0700

    add create ingress for service

--- README.md
+++ README.md
@@ -4,6 +4,25 @@
  - Trying your hand and writing your own kubernetes controllers,
  - Learning how logging, debugging, etc. works on k8s
 
+
+minikube start
+kubectl create ns eksposetest
+kubectl create deployment nginx -n eksposetest --image nginx
+kubectl delete deployment -n eksposetest nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.46.0/deploy/static/provider/do/deploy.yaml
+kubectl delete all --all -n ekspose
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.46.0/deploy/static/provider/do/deploy.yaml
+kubectl get all -n ingress-nginx
+kubectl get nodes
+kubectl get deployments
+kubectl create deployment nginx -n eksposetest --image nginx
+kubectl get deployments
+kubectl create deployment hello-node --image=registry.k8s.io/e2e-test-images/agnhost:2.39 -- /agnhost netexec --http-port=8080
+kubectl get service -n testone
+kubectl get ingress -n testone
+kubectl get service -n ingress-nginx
+
 ## References
 https://cloud.tencent.com/developer/article/1493250
 https://github.com/kubernetes/sample-controller
--- ekspose/controller.go
+++ ekspose/controller.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
+	coreV1 "k8s.io/api/core/v1"
+	netV1 "k8s.io/api/networking/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	appsinformers "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
@@ -31,8 +36,8 @@ func newController(clientset kubernetes.Interface, depInformer appsinformers.Dep
 	// Register functions in informer to handle add/delete events
 	depInformer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    handleAdd,
-			DeleteFunc: handleDel,
+			AddFunc:    c.handleAdd,
+			DeleteFunc: c.handleDel,
 		},
 	)
 	return c
@@ -55,16 +60,134 @@ func (c *controller) run(ch <-chan struct{}) {
 	<-ch
 }
 
+// The worker will keep running processItem until it returns false
 func (c *controller) worker() {
+	for c.processItem() {
 
+	}
+}
+
+// Poll from the queue and then sync deployment
+func (c *controller) processItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer c.queue.Forget(item)
+	key, err := cache.MetaNamespaceKeyFunc(item)
+	if err != nil {
+		fmt.Printf("getting key from cache %s\n", err.Error())
+	}
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		fmt.Printf("splitting key into namespace and name %s\n", err.Error())
+		return false
+	}
+
+	err = c.syncDeployment(ns, name)
+	if err != nil {
+		//retry
+
+		fmt.Printf("syncing deployment %s\n", err.Error())
+		return false
+	}
+	return true
+}
+
+// Sync deployment: create service and ingress
+func (c *controller) syncDeployment(ns, name string) error {
+	ctx := context.Background()
+
+	dep, err := c.depLister.Deployments(ns).Get(name)
+	if err != nil {
+		fmt.Printf("getting deployment from lister %s\n", err.Error())
+	}
+
+	// create service
+	// We have to modify this, to figure out the port
+	// the deployment container is listening on.
+	svc := coreV1.Service{
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      dep.Name,
+			Namespace: ns,
+		},
+		Spec: coreV1.ServiceSpec{
+			// If without a selector, it will have errors
+			Selector: depLables(*dep),
+			Ports: []coreV1.ServicePort{
+				{
+					Name: "http",
+					Port: 80,
+				},
+			},
+		},
+	}
+	s, err := c.clientset.CoreV1().Services(ns).Create(ctx, &svc, metaV1.CreateOptions{})
+	if err != nil {
+		fmt.Printf("creating service %s\n", err.Error())
+	}
+
+	//create ingress
+	return createIngress(ctx, c.clientset, s)
+}
+
+// Create ingress resources after service is created
+func createIngress(ctx context.Context, client kubernetes.Interface, svc *coreV1.Service) error {
+
+	pathType := "Prefix"
+	ingress := netV1.Ingress{
+		TypeMeta: metaV1.TypeMeta{},
+		ObjectMeta: metaV1.ObjectMeta{
+			Name:      svc.Name,
+			Namespace: svc.Namespace,
+			Annotations: map[string]string{
+				"nginx.ingress.kubernetes.io/rewrite-target": "/",
+			},
+		},
+		Spec: netV1.IngressSpec{
+			Rules: []netV1.IngressRule{
+				{
+					IngressRuleValue: netV1.IngressRuleValue{
+						HTTP: &netV1.HTTPIngressRuleValue{
+							Paths: []netV1.HTTPIngressPath{
+								{
+									Path:     fmt.Sprintf("/%s", svc.Name),
+									PathType: (*netV1.PathType)(&pathType),
+									Backend: netV1.IngressBackend{
+										Service: &netV1.IngressServiceBackend{
+											Name: svc.Name,
+											Port: netV1.ServiceBackendPort{
+												Number: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		Status: netV1.IngressStatus{},
+	}
+	_, err := client.NetworkingV1().Ingresses(svc.Namespace).Create(ctx, &ingress, metaV1.CreateOptions{})
+	return err
 }
 
 // Add handler: Add obj to queue
-func handleAdd(obj interface{}) {
+func (c *controller) handleAdd(obj interface{}) {
 	fmt.Println("Add called")
+	// Add obj to queue
+	c.queue.Add(obj)
 }
 
 // Del handler: Add obj to queue
-func handleDel(obj interface{}) {
+func (c *controller) handleDel(obj interface{}) {
 	fmt.Println("Del called")
+	// Add obj to queue
+	c.queue.Add(obj)
+}
+
+func depLables(dep appsv1.Deployment) map[string]string {
+	return dep.Spec.Template.Labels
 }
